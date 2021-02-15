6b81a8cdef9ec7f77c8816059507dd949d78d259
NTL <58549685+brad0520@users.noreply.github.com>
2021-02-15 20:57:58 +0900

    Update AccountingApp.java

--- AccountingApp.java
+++ AccountingApp.java
@@ -1,29 +1,18 @@
 
-public class AccountingIFApp {
+public class AccountingApp {
 
 	public static void main(String[] args) {
 		
-		double valueOfSupply = 20000.0;
+		double valueOfSupply = 10000.0;
 		double vatRate = 0.1;
 		double expenseRate = 0.3;
 		double vat = valueOfSupply*vatRate;
 		double total = valueOfSupply + vat;
 		double expense = valueOfSupply*expenseRate;
 		double income = valueOfSupply - expense;
-		
-		double dividend1;
-		double dividend2;
-		double dividend3;
-
-		if(income > 10000.0) {
-			dividend1 = income * 0.5;
-			dividend2 = income * 0.3;
-			dividend3 = income * 0.2;
-		} else {
-			dividend1 = income * 1;
-			dividend2 = income * 0;
-			dividend3 = income * 0;
-		}
+		double dividend1 = income * 0.5;
+		double dividend2 = income * 0.3;
+		double dividend3 = income * 0.2;
 		
 		System.out.println("Value of supply : "+ valueOfSupply);
 		System.out.println("VAT : " + vat );
