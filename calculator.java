4e17da11186cc71558bc4f575bc512ae5227eab3
NTL <58549685+brad0520@users.noreply.github.com>
2021-02-17 10:50:30 +0900

    update code
    update some codes

--- calculator.java
+++ calculator.java
@@ -6,6 +6,8 @@ public class calculator {
 
 	public static void main(String[] args) {
 		while (true) {
+		System.out.println();
+		System.out.println("-------------------------------------------------------------------");
 		System.out.println("원하는 기능을 선택해주세요  (1. 더하기, 2. 빼기, 3. 곱하기, 4. 나누기, 5. 종료):");
 		
 		Scanner sc = new Scanner(System.in);
@@ -13,6 +15,7 @@ public class calculator {
 		
 		if (select==1 ) {
 			System.out.println("덧셈을 수행합니다.");
+			System.out.println();
 			System.out.println("첫번째 숫자를 입력해주세요.");
 			int num1 = Integer.parseInt(sc.nextLine());	
 			System.out.println("두번째 숫자를 입력해주세요.");
@@ -20,6 +23,7 @@ public class calculator {
 			System.out.println(num1 +" + " + num2 + " = " + num1+num2);
 		} else if (select==2 ) {
 			System.out.println("뺄셈을 수행합니다.");
+			System.out.println();
 			System.out.println("첫번째 숫자를 입력해주세요.");
 			int num1 = Integer.parseInt(sc.nextLine());	
 			System.out.println("두번째 숫자를 입력해주세요.");
@@ -27,6 +31,7 @@ public class calculator {
 			System.out.println(num1 +" - " + num2 + " = " + (num1-num2));
 		} else if (select==3) {
 			System.out.println("곱셈을 수행합니다.");
+			System.out.println();
 			System.out.println("첫번째 숫자를 입력해주세요.");
 			int num1 = Integer.parseInt(sc.nextLine());	
 			System.out.println("두번째 숫자를 입력해주세요.");
@@ -34,12 +39,17 @@ public class calculator {
 			System.out.println(num1 +" * " + num2 + " = " + num1*num2);
 		} else if (select==4) {
 			System.out.println("나눗셈을 수행합니다.");
+			System.out.println();
 			System.out.println("첫번째 숫자를 입력해주세요.");
 			int num1 = Integer.parseInt(sc.nextLine());	
 			System.out.println("두번째 숫자를 입력해주세요.");
 			int num2 = Integer.parseInt(sc.nextLine());	
 			System.out.println(num1 +" / " + num2 + " = " + num1/num2);
 		} else {
+			System.out.println();
+			System.out.println("계산기를 종료합니다.");
+			System.out.println();
+			System.out.println("-------------------------------------------------------------------");
 			break;
 		}
 		}
