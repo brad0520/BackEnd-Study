fd7489414366a34dfe579a1a7e57fb0fde7ba4bf
NTL <58549685+brad0520@users.noreply.github.com>
2021-02-23 17:58:18 +0900

    fixing code
     fix code efficiently

--- arrayShuffle.java
+++ arrayShuffle.java
@@ -8,10 +8,10 @@ public class arrayShuffle {
 		int[] numArr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 		System.out.println(Arrays.toString(numArr));
 
-		for (int i=1; i<100; i++) {
+		for (int i=0; i < numArr.length; i++) {
 			int n = (int) (Math.random()*10);
-			int temp = numArr[0];
-			numArr[0] = numArr[n];
+			int temp = numArr[i];
+			numArr[i] = numArr[n];
 			numArr[n] = temp;
 		}
 
