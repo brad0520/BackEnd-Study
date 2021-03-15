b828fa3a92746132e7d313120201bd2da6a4bac8
brad0135 <brad0135@gmail.com>
2021-03-15 22:29:23 +0900

    from local

---
+++ Reply.java
@@ -0,0 +1,42 @@
+package boardApp_chaT;
+
+// 데이터 구조화 클래스 - DTO, VO
+public class Reply {
+	
+	private int id;
+	private int parentId;
+	private String body;
+	private String nickname;
+	private String regDate;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getParentId() {
+		return parentId;
+	}
+	public void setParentId(int parentId) {
+		this.parentId = parentId;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+}
---
+++ Test.java
@@ -0,0 +1,10 @@
+package boardApp_chaT;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		App app = new App();
+		app.start();
+	}
+
+}
